afe0a7a60bb2650440afb31c4306e9dec4d45c52
SHIVAM PATRA <142797289+shivampatra@users.noreply.github.com>
2026-02-16 10:55:54 +0530

    Implement loop to print underscores in main method
    Add a loop to print underscores ten times.

--- main.java
+++ main.java
@@ -1,5 +1,7 @@
 public class main{
     public static void main(String[] args) {
         System.out.println("Hello World");
+        for(int i=0;i<10;i++){
+        System.out.print("__")
     }
 }
