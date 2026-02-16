adf5126977e15de79b4b24851fcb8deefe8c844a
SHIVAM PATRA <142797289+shivampatra@users.noreply.github.com>
2026-02-16 11:09:03 +0530

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,6 @@ public class main{
     public static void main(String[] args) {
         System.out.println("Hello World");
         for(int i=0;i<10;i++){
-        System.out.print("__")
+        System.out.print("__");
     }
 }
